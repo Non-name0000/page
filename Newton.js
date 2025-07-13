e9524bf43923b9218d8a1d33ce9b319ed7e900fc
Non-name0000 <s2332138fl@s.chibakoudai.jp>
2025-07-13 22:04:50 +0900

    Update Newton.js

--- Newton.js
+++ Newton.js
@@ -1,11 +1,31 @@
 class Newton{
+  
   function(n){
-    return 1/4*n(x-n)-3/2
+    let line;
+    line=1/4*n(x-n)-1/8*n*n-2;
+    return line;
   }
   
   let a=document.querySelector('#startpoint1');
-  let b=document.querySelector('#startpoint2'); 
+  for(a<10){
+    if(function(a)==0){
+    b=a;
+    }
+    a=a+0.01;
+  }
   let EPS=document.querySelector('#EPS');
-  
+  let count=0;
+  while(function(b)<EPS){
+    a=b;
+  for(a<=10){
+    if(function(a)==0){
+    b=a;
+    }
+    a=a+0.01;
+    count=count++;
+  }
+  }
+}
+    
   
   
