1da699e54dc0566f8afcd2aa5951482fa215e761
Non-name0000 <s2332138fl@s.chibakoudai.jp>
2025-07-13 19:25:51 +0900

    Update Nibun.js

--- Nibun.js
+++ Nibun.js
@@ -6,17 +6,20 @@ class Nibun{
   
   let a=document.querySelector('#startpoint1');
   let b=document.querySelector('#startpoint2'); 
-  let c=(a+b)/2;
   let EPS=document.querySelector('#EPS');
+  let count=0;
+  
+  while(function(a)-function<EPS){
+  let c=(a+b)/2;
   /*未完成部分*/
   /*let Aanswer=document.querySelector('#answer1');
   let Banswer=document.querySelector('#answer2');
   let C=document.querySelector('#C');*/
   /*ここまで*/
-  while(function(a)-function<EPS){
   if(function(a)*function(c)<0){
     b=c;
   }else if(function(b)*function(c)<0){
   　a=c;
 　}
+  count++;
 }
