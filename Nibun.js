db4213d5fabf2297df5260a81edebbd4a1d7c526
Non-name0000 <s2332138fl@s.chibakoudai.jp>
2025-07-13 17:29:32 +0900

    Update Nibun.js

--- Nibun.js
+++ Nibun.js
@@ -1,12 +1,13 @@
 class Nibun{
 
   function nibun(n){
-    return 1/8*a*a-2;
+    return 1/8*n*n-2;
   }
-    
-  let a,b,c;
   
-  
+  let a=document.getElementById("startpoint1");
+  let b=document.getElementById("startpoint2");
+  let c=(a+b)/2;
+  if(nibun(a)*nibun(c)<0)
 
   
 
