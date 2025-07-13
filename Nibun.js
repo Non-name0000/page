a9e97b78a15dd7205436f701bce206d6c96a3786
Non-name0000 <s2332138fl@s.chibakoudai.jp>
2025-07-13 17:19:26 +0900

    Update Nibun.js

--- Nibun.js
+++ Nibun.js
@@ -1,12 +1,12 @@
 class Nibun{
 
-  function nibun(a,b,c){
-    a=1/8*a*a-2;
-    b=1/8*b*b-2;
-    c=1/8*b*b-2;
-    if(a*c<0){
-      
+  function nibun(n){
+    return 1/8*a*a-2;
   }
+    
+  let a,b,c;
+  
+  
 
   
 
